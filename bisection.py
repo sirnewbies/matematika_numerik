2b99f87337234be2035a3e19d30f7df5d5dcea62
sirNewbies <yusromi04@gmail.com>
2024-03-05 20:43:28 +0700

    Perbaiki tampilan agar mudah dibaca

--- bisection.py
+++ bisection.py
@@ -3,8 +3,8 @@ def fungsi(nilai_x):
 
 epsilon = 0.0001
 
-xn = int(input("masukan xn: "))
-xn_tambah_1 = int(input("masukan xn + 1: "))
+xn = float(input("Masukkan nilai xn: "))
+xn_tambah_1 = float(input("Masukkan nilai xn + 1: "))
 print("")
 
 fxn = fungsi(xn)
@@ -12,29 +12,32 @@ fxn_tambah_1 = fungsi(xn_tambah_1)
 
 xt = fxn * fxn_tambah_1 < 0
 
+iterasi = 1
+
 while xt:
     xt = (xn + xn_tambah_1) / 2
-    print("nilai xt: ", xt)
     fxt = fungsi(xt)
+    
+    print("Iterasi ke-:", iterasi)
+    print("Nilai xn:", xn)
+    print("Nilai xn+1:", xn_tambah_1)
+    print("Nilai xt:", xt)
+    print("Nilai f(xn):", fxn)
+    print("Nilai f(xn+1):", fxn_tambah_1)
+    print("Nilai f(xt):", fxt)
+    print("")
     
     if fxt * fxn < 0:
         xn_tambah_1 = xt
         fxn_tambah_1 = fxt 
-        print("nilai xn: ", xn)
-        print("nilai xn+1: ", xn_tambah_1)
-        print("nilai xt: ", xt)
-        print("nilai f(xn): ", fxn)
-        print("nilai f(xn+1): ", fxn_tambah_1)
-        print("nilai f(xx)2: ", fxt)
-        print("")
-    
-    else :
+        
+    else:
         xn = xt
         fxn = fxt
-        print("nilai xn: ", xn)
-        print("nilai fxn: ", fxn)
         
-    if abs(fxt) <= epsilon :
+    if abs(fxt) <= epsilon:
         akar = xt
-        print("Akar: ",akar)
+        print("Akar:", akar)
         break
+        
+    iterasi += 1
